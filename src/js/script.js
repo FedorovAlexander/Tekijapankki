c4edc723667d842f8f13d456d7f1b75042888d39
alex <hamster2240@gmail.com>
2020-02-03 23:48:26 +0300

    Make header adaptive. First iteration

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,7 @@
 svg4everybody(); // иницализация полифила для IE
 
-$(document).ready(function(){
-  // весь ваш код c jQuery
+$(document).ready(function() {
+  $('.header__menu-icon').on('click', function() {
+    $('.header__nav-wrapper--mobile').toggleClass('active');
+  })
 });
-
-// Если на проекте нет jQuery, но хочется $( document ).ready... (IE9+)
-// function ready(fn) {
-//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
-//   }
-// }
-//
-// ready(function(){
-//   // code
-// });
