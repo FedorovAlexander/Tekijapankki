ba6d42fa6a8e3b431e78b136673bbe6901099d95
alex <hamster2240@gmail.com>
2020-02-05 07:12:47 +0300

    Add a function for counting list items

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,30 @@
 svg4everybody(); // иницализация полифила для IE
 
 $(document).ready(function() {
+
   $('.header__menu-icon').on('click', function() {
     $('.header__nav-wrapper--mobile').toggleClass('active');
   })
+
+  function addToList() {
+    $('.-js-add .button').on('click', function(e) {
+      e.preventDefault();
+      $(this).toggleClass('active');
+      $(this).text(function(i, text) {
+        return text === "Pyydä tarjous!" ? "Lisätty listalle" : "Pyydä tarjous!";
+      })
+      $('.-js-count-button').find('.button').addClass('active');
+
+      if (!$(".-js-add .button.active").length) {
+        $('.-js-count-button').find('.button').removeClass('active');
+      }
+      if ($(".-js-add .button.active").length) {
+        $('.-js-counter').text($(".-js-add .button.active").length)
+      } else {
+        $('.-js-counter').text('');
+      }
+
+    })
+  }
+  addToList();
 });
